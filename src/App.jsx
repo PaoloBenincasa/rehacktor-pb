a4f0723ffe25ccc8ffa06082fdf72604d87152b7
Paolo Benincasa <paolobenincasa1984@gmail.com>
2025-01-15 09:36:43 +0100

    provaprova

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router";
 import router from "./routes/Routes";
 import SessionContextProvider from "./context/SessionContextProvider";
 import GameContextProvider from "./Components/GameContext/GameContextProvider";
-import FavContextProvider from "./Components/favContext/favContextProvider";
+import FavContextProvider from "./Components/favContext/FavContextProvider";
 
 function App() {
   return (
